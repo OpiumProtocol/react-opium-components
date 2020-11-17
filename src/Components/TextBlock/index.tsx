8fe76b4b98cf3aeb2b1a86357aab68026199cc32
mkunak <kunak.n@gmail.com>
2020-11-17 13:20:39 +0200

    mk->os-47: textblock component

--- src/Components/TextBlock/index.tsx
+++ src/Components/TextBlock/index.tsx
@@ -1,4 +1,4 @@
-import React, { DetailedHTMLProps, FC, HTMLAttributes } from 'react'
+import React, { FC } from 'react'
 import { v4 as uuidv4 } from 'uuid'
 
 import { generateRenderProps } from '../../Utils/helpers'
