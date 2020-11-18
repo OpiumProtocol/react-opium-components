a27e6ef23770eaaa7f70ed76619937d593a4f6cf
mkunak <kunak.n@gmail.com>
2020-11-18 13:23:50 +0200

    mk->os-48: Loading component

---
+++ src/Components/Loading/Loading.stories.tsx
@@ -0,0 +1,14 @@
+import React, { useState } from 'react'
+import Loading from './index'
+
+import { withKnobs, text } from '@storybook/addon-knobs'
+
+export const loading = () => {
+  text('Text', 'Loading!')
+
+  return (
+    <Loading />
+  )
+}
+
+export default { title: 'Loading component', decorators: [withKnobs] }
--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import Tabs from './Components/Tabs'
 import Alert from './Components/Alert'
 import Table from './Components/Table'
 import Button from './Components/Button'
+import Loading from './Components/Loading'
 import Segments from './Components/Segments'
 import TextBlock from './Components/TextBlock'
 import LokalizedInput from './Components/LokalizedInput'
@@ -12,6 +13,7 @@ export {
   Alert,
   Table,
   Button,
+  Loading,
   Segments,
   TextBlock,
   LokalizedInput,
