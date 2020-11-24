5ddbe0d6f4cf9b19f4b1dc5e1cfd62a8ddc3ba42
mkunak <kunak.n@gmail.com>
2020-11-24 20:24:49 +0200

    mk->fixes: TextBlock changes

--- src/Components/TextBlock/index.tsx
+++ src/Components/TextBlock/index.tsx
@@ -36,7 +36,7 @@ const TextBlock: FC<Props> = (props: Props) => {
     <div className={`text-block color-scheme-${theme} ${className}`} style={styles}>
       {
         content.map((contentLine: string) => (
-          <div key={uuidv4()} dangerouslySetInnerHTML={createMarkup(contentLine)} />
+          <div key={uuidv4()} dangerouslySetInnerHTML={createMarkup(contentLine)} style={{width: '100%'}} />
         ))
       }
     </div>
