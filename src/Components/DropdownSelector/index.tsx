d7b58806f081b83642a97d957971ff9efc7bda76
mkunak <kunak.n@gmail.com>
2020-11-25 15:33:11 +0200

    mk->fixes: dropdown selector (controlled-uncontrolled)

--- src/Components/DropdownSelector/index.tsx
+++ src/Components/DropdownSelector/index.tsx
@@ -10,21 +10,24 @@ import './DropdownSelector.scss'
 export type Props = {
   /** Define theme */
   theme?: Theme
-  /** Set initial droping down option */
-  initialOption: string
-  /** Set droping down options */
-  items?: { text: string, value: string }[]
+  /** Title, if controlled */
+  title?: Theme
+  /** Value passed to the onSelect handler, useful for identifying the selected menu item */
+  eventKey?: any
+  /** Set dropping down options */
+  items?: { title: string, value: string }[]
   /** Function, that became active by clicking on an option */
   onClick?: () => void
   /** Function, that became active after an option has become selected */
   onSelect?: (eventKey: any, event: BaseSyntheticEvent) => any
   /** Set class selectors */
   className?: string
+  /** Set class selectors */
+  isControlled?: boolean
 }
 
 const defaultProps: Props = {
   theme: Theme.DARK,
-  initialOption: '',
   items: [],
   className: '',
   onClick: () => { },
@@ -33,27 +36,39 @@ const defaultProps: Props = {
 
 const DropdownSelector: FC<Props> = (props: Props) => {
   const renderProps = generateRenderProps(defaultProps, props)
-  const { initialOption, items, onClick, onSelect, theme, className } = renderProps
+  const {
+    title,
+    items,
+    theme,
+    onClick,
+    onSelect,
+    className,
+    uncontrolled,
+  } = renderProps
+
+  const [selectorTitle, setSelectorTitle] = useState<any>(items[0].title)
+
+  const handleSelect = (key: any, event: BaseSyntheticEvent) => {
+    setSelectorTitle(event.target.innerText)
+  }
 
   return (
     <Dropdown className={`color-scheme-${theme} ${className}`}>
       <Dropdown.Toggle id="dropdown-selector-toggle" className={`color-scheme-${theme}`}>
-        {'Dropdown Title'}
+        {title || selectorTitle}
       </Dropdown.Toggle>
       <Dropdown.Menu className={`color-scheme-${theme}`}>
-        <Dropdown.Item onClick={() => { }} className={`color-scheme-${theme}`} >
-          {initialOption}
-        </Dropdown.Item>
         {
-          items.map((el: any) => (
+          items.map((item: any, idx: number) => (
             <Dropdown.Item
               key={uuidv4()}
-              eventKey={el.value}
+              eventKey={`${idx}`}
+              href={item.href}
               onClick={onClick}
-              onSelect={onSelect}
+              onSelect={uncontrolled ? handleSelect : onSelect}
               className={`color-scheme-${theme}`}
             >
-              {el.text}
+              {item.title}
             </Dropdown.Item>
           ))
         }
