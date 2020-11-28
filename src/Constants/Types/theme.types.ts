e225507349721c7835d4a9421df76060509f73ee
Timur <tima950203@gmail.com>
2020-11-29 00:04:28 +0100

    first test

--- src/Constants/Types/theme.types.ts
+++ src/Constants/Types/theme.types.ts
@@ -22,15 +22,15 @@ type TColorSet = {
 
 export const colorSchemeLight: TColorScheme = {
   color: {
-    primary: { value: _.blue1, hover: _.white0 },
-    secondary: { value: _.gray4, hover: _.white0 },
+    primary: { value: _.gray2, hover: _.white0 },
+    secondary: { value: _.gray3, hover: _.white0 },
     danger: { value: _.red0, hover: _.red2 },
     success: { value: _.gray4, hover: _.green1_darken },
     warning: { value: _.gray4, hover: _.yellow3_darken },
   },
   backgroundColor: {
     primary: { value: _.white0, hover: _.blue1 },
-    secondary: { value: _.gray6_30, hover: _.gray5 },
+    secondary: { value: _.white1, hover: _.gray5 },
     danger: { value: _.red0_20, hover: _.red0_40 },
     success: { value: _.green1_30, hover: _.green1_50 },
     warning: { value: _.yellow3_60, hover: _.yellow3 },
@@ -47,15 +47,15 @@ export const colorSchemeLight: TColorScheme = {
 
 export const colorSchemeDark: TColorScheme = {
   color: {
-    primary: { value: _.darkgray4, hover: _.white0 },
-    secondary: { value: _.darkgray4, hover: _.darkgray6 },
+    primary: { value: _.darkgray6, hover: _.white0 },
+    secondary: { value: _.darkgray7, hover: _.darkgray6 },
     danger: { value: _.red0_50, hover: _.red0 },
     success: { value: _.green1, hover: _.white0 },
     warning: { value: _.yellow3, hover: _.white0 },
   },
   backgroundColor: {
-    primary: { value: _.darkgray1, hover: _.darkgray1_darken },
-    secondary: { value: _.darkgray2, hover: _.darkgray3 },
+    primary: { value: _.darkBlue1, hover: _.darkgray1_darken },
+    secondary: { value: _.darkgray1, hover: _.darkgray3 },
     danger: { value: _.red0_20, hover: _.red0_40 },
     success: { value: _.green1_30, hover: _.green1_40 },
     warning: { value: _.yellow3_60, hover: _.yellow3_70 },
