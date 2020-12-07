d94e3bd9cb8148892e9452cc9153a66811490764
mkunak <kunak.n@gmail.com>
2020-12-07 19:19:23 +0200

    mk>os-76: gtm component

---
+++ src/Components/GTMWrapper/index.tsx
@@ -0,0 +1,35 @@
+import React, { FC, cloneElement } from 'react'
+
+import { generateRenderProps } from '../../Utils/helpers'
+
+import './GTMWrapper.scss'
+
+export type TProps = {
+  /** ID */
+  id: string
+  /** Wrapped children */
+  children: JSX.Element
+  /** Is production mode */
+  isProduction: boolean
+}
+
+export const defaultProps: TProps = {
+  id: 'ceo-wrapped-component',
+  children: <></>,
+  isProduction: false,
+}
+
+const GTMWrapper: FC<TProps> = (props: TProps) => {
+  const {
+    id,
+    children,
+    isProduction,
+  } = generateRenderProps(defaultProps, props)
+  
+  return isProduction ? cloneElement(children, { id }) : children
+
+  // const modifyChildren = isProduction ? () => cloneElement(children, { id }) : () => (children)
+  // return modifyChildren()
+}
+
+export default GTMWrapper
