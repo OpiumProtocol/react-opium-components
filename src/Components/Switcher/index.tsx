13baa1489acdc35660f833b28d429d45f9d60c37
Ponochovny <ponochovny@gmail.com>
2021-04-03 00:26:02 +0300

    comments

--- src/Components/Switcher/index.tsx
+++ src/Components/Switcher/index.tsx
@@ -8,10 +8,15 @@ import './Switcher.scss'
 export type Props = {
   /** Define theme */
   theme?: ETheme
+  /** To set the target */
   id: string
+  /** Label of current switcher */
   label?: string
+  /** For controlled variant. uncontrolled is false (Default) */
   onChange?: (value: boolean) => void
+  /** For uncontrolled variant. uncontrolled is true */
   checked?: boolean
+  /** If true - you can use 'checked' and control current switcher with it */
   uncontrolled?: boolean
 }
 
