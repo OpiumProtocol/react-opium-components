6804ad81a691fa240268ef232cdb9662e5fe5523
Timur <tima950203@gmail.com>
2021-11-26 18:02:50 +0100

    labels

--- src/Components/LineChart/index.tsx
+++ src/Components/LineChart/index.tsx
@@ -118,7 +118,7 @@ const LineChart: React.FC<Props> = (props: Props) => {
           </defs>
           <CartesianGrid vertical={false} stroke={theme === ETheme.DARK ? 'rgba(255, 255, 255, 0.15)' : 'rgba(10, 10, 30, 0.15)'} />
           <XAxis dataKey="label" scale="band" label={labelX} interval={intervalX} tickFormatter={tickFormatterX} domain={domainX} height={50} tick={{ dx: 20 }}/>
-          <YAxis label={labelY} tickFormatter={tickFormatterY} allowDataOverflow domain={domainY} tick={{ dx: -10 }}/>
+          <YAxis label={labelY} tickFormatter={tickFormatterY} allowDataOverflow domain={domainY} tick={{ dx: -20 }}/>
           <Tooltip content={<CustomTooltip />} />
           <Area
             type="monotone"
