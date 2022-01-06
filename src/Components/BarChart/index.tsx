5353559629fe7cdc4b67894da8a2041097f9e100
eganichev <evgenii.ganichev@gmail.com>
2022-01-06 17:08:52 +0200

    Remove BarChart data logging

--- src/Components/BarChart/index.tsx
+++ src/Components/BarChart/index.tsx
@@ -80,7 +80,6 @@ type ICustomTooltip = {
 
 const OpiumBarChart: React.FC<Props> = (props: Props) => {
   const renderProps = generateRenderProps(defaultProps, props)
-  console.log('OpiumBarChart renderProps', renderProps)
 
   const {
     theme,
@@ -103,9 +102,6 @@ const OpiumBarChart: React.FC<Props> = (props: Props) => {
   } = renderProps
 
   const CustomTooltip = ({ payload, active }: ICustomTooltip) => {
-    console.log('CustomTooltip payload', payload)
-    console.log('CustomTooltip active', active)
-
     if (active) {
       if (payload && payload[0].payload.customTooltip) {
         return (
