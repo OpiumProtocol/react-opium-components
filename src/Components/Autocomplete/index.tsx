a694114d67321d2ddaf59f51159521861983f7ee
eganichev <evgenii.ganichev@gmail.com>
2022-02-11 11:11:43 +0200

    Changed styles

--- src/Components/Autocomplete/index.tsx
+++ src/Components/Autocomplete/index.tsx
@@ -37,9 +37,10 @@ const defaultProps: Props = {
 // @ts-ignore
 const CustomToggle = React.forwardRef(({ children, onClick }: {children: any, onClick: (e: any) => void }, ref) => (
   // @ts-ignore
-  <button
+  <div
     // @ts-ignore
     ref={ref}
+    className="DropDown__toggle"
     onClick={(e) => {
       e.preventDefault()
       onClick(e)
@@ -52,7 +53,7 @@ const CustomToggle = React.forwardRef(({ children, onClick }: {children: any, on
     <svg className="DropDown__arrow" width="14" height="8" viewBox="0 0 14 8" fill="none" xmlns="http://www.w3.org/2000/svg">
       <path d="M6 6L0 0L12 8.80993e-07L6 6Z" fill="#999BBC"/>
     </svg>
-  </button>
+  </div>
 ))
 
 CustomToggle.displayName = 'CustomToggle'
