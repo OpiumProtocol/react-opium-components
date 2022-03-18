2f4e807e07700a4d6460446f7f19c05196067cfe
eganichev <evgenii.ganichev@gmail.com>
2022-03-18 16:04:44 +0200

    Added media queries

--- src/Components/StaticChart/LongStrangleChart/index.tsx
+++ src/Components/StaticChart/LongStrangleChart/index.tsx
@@ -275,9 +275,9 @@ const LongStrangleChart: FC<TProps> = (props: TProps) => {
         <ReferenceLine strokeWidth={0.5} stroke='#C4C4C4' segment={[{ x: 20, y: -1.3 }, { x: 20, y: 1.3 }]} />
         <ReferenceLine stroke="white" strokeWidth="0.5" segment={[{ x: 0, y: -1.3 }, { x: 0, y: 1.3 }]} />
         <ReferenceLine strokeOpacity={0.2} strokeWidth={1} stroke='#C4C4C4' segment={[{ x: 30, y: -1.3 }, { x: 30, y: 1.3 }]} />
-        <ReferenceArea x1={5} x2={15} y1={0.015} y2={0.3} fill={'rgba(10, 10, 30, 0.7)'} />
-        <ReferenceArea x1={15} x2={25} y1={-0.3} y2={-0.015} fill={'rgba(10, 10, 30, 0.7)'} />
-        <ReferenceArea x1={26} x2={34} y1={0.015} y2={0.3} fill={'rgba(10, 10, 30, 0.7)'} />
+        <ReferenceArea x1={5} x2={15} y1={0.015} y2={0.3} fill={'rgba(10, 10, 30, 1)'} />
+        <ReferenceArea x1={15} x2={25} y1={-0.3} y2={-0.015} fill={'rgba(10, 10, 30, 1)'} />
+        <ReferenceArea x1={26} x2={34} y1={0.015} y2={0.3} fill={'rgba(10, 10, 30, 1)'} />
         <Tooltip content={<CustomTooltip />} />
         <XAxis
           height={50}
