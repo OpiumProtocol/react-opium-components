1177c1da732d1a23edd55f4fb5aa139c9814a845
eganichev <evgenii.ganichev@gmail.com>
2022-05-02 14:44:35 +0300

    Changed styles imports

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,4 +1,4 @@
-import '!style-loader!css-loader!sass-loader!../src/Styles/index.scss';
+// import '!style-loader!css-loader!sass-loader!../src/Styles/index.scss';
 
 export const parameters = {
     controls: { expanded: true },
